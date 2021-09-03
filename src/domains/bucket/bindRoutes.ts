732a174ac5559535b0ac7cdc4e18772be44e9f76
Matheus Lima Machado <matmachado@uolinc.com>
2021-09-02 20:56:40 -0400

    feat: create validation.
    Co-authored-by: Ângela Ferreira <angelarf@users.noreply.github.com>

--- src/domains/bucket/bindRoutes.ts
+++ src/domains/bucket/bindRoutes.ts
@@ -4,12 +4,13 @@ import registerRouter from "../../commons/registerRouter"
 
 import getAllByUserId from "./controllers/getAllByUserId"
 import create from "./controllers/create"
+import createValidator from "./validators/createValidator"
 
 const router = express.Router()
 
 // should the userId be exposed in the route?
 router.get('/', getAllByUserId)
-router.post('/', create)
+router.post('/', createValidator, create)
 
 const bindRoutesToApp = registerRouter({
   domain: "/buckets",
--- src/domains/bucket/controllers/create.ts
+++ src/domains/bucket/controllers/create.ts
@@ -12,20 +12,10 @@ type Bucket = {
   dueDate?: Date
 }
 
-const validateBody = (bucket: Bucket) => bucket
-
 export default async (req: Request, res: Response): Promise<Response> => {
   const newBucket = req?.body
 
-  // where should validation be?
-  // to do: validate received body
-  if (validateBody(newBucket)) {
-
-    // but wait! Were is the model? 
-    // Who's resposible for call the repositories functions? 
-    createBucket(newBucket)
-    return res.status(201).send(newBucket)
-  }
-
-  return res.status(422).send({ message: "Invalid bucket" })
+  createBucket(newBucket)
+  
+  return res.status(201).send(newBucket)
 }
